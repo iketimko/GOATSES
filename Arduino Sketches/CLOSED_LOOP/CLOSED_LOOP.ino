5602c25a2570a2d7bc16b6e4176e29b6e24c87de
unknown <cowh9052@colorado.edugit config --global user.email cowh9052@colorado.edulscd>
2022-03-10 13:47:22 -0700

    Integrated Stepper Control into CLOSED_LOOP

--- Arduino Sketches/CLOSED_LOOP/CLOSED_LOOP.ino
+++ Arduino Sketches/CLOSED_LOOP/CLOSED_LOOP.ino
@@ -1,3 +1,24 @@
+/* 
+QHM + Cody Wheeler + Ike Timko
+Using igus dryve D7 Stepper Motor Control System, Using Bendlabs angle sensor data read and calibration
+For BendLabs:
+SCL to SCL
+SDA to SDA
+3.3V IN
+Ground
+
+For Stepper:
+Ground
+Pin 6 is EN+
+Pin 7 is DIR+
+Pin 8 is STEP+
+GND connects to EN-, DIR-, and STEP-
+On the D7 Controller, the motor will connect to A+/A-/B+/B-
+Power source connects to V+/V-
+Switches 4, 5, 6, 8 set ON
+
+*/
+
 #include <Arduino.h>
 #include <Ewma.h>
 #include <Wire.h>
@@ -7,6 +28,12 @@ Ewma filtered_data(0.01);   // Exponentially Weighted Moving Average
 ADS myFlexSensor; //Create instance of the Angular Displacement Sensor (ADS) class
 byte deviceType; //Keeps track of if this sensor is a one axis of two axis sensor
 float data;
+const int StepsPerRev = 200;  // steps per revolution on the stepper motor
+const float DistPerStep = 0.012; //distance moved by actuator for each step of the motor, in inches
+int StepCount = 0; //initialize step counter
+
+  // TEST MOTION VALUES, REMOVE WITH COMMANDS
+  float dx = DistPerStep*10;
 
 void setup()
 {
@@ -28,12 +55,49 @@ void setup()
   }
   
   calibrate();
+
+  // Pause After Sensor Calibration to begin test
+  Serial.println(F("Good. Now press a key when ready to begin the test."));
+  while (Serial.available() == 0)
+  {
+    myFlexSensor.available();
+    delay(10); //Wait for user to press character
+  }
+  
   // END Bendlabs sensor
+  // **************************
+
+  // **************************
+  // For Stepper Motor Control
+  
+  // set up pins as outputs
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  
+  //set EN+ to positive
+  digitalWrite(6, HIGH);
+  digitalWrite(7, LOW);
+  digitalWrite(8, LOW);
+
+  delay(5); //wait 5 ms
+
+  // END Stepper Motor Control
+  // **************************
+
+  // **************************
+  // For Stepper Motor PID
+
+
+  // END Stepper Motor PID
   // **************************
 }
 
 void loop()
 {
+  // **************************
+  // For Bendlabs Calibration and data reading
+  
   // get bandlabs sensor data
   if (myFlexSensor.available() == true)
   {
@@ -43,10 +107,25 @@ void loop()
     Serial.println();
   }
 
+  // END BendLabs Calibration and Data Reading
+  // **************************
+
+  // **************************
+  // For Stepper Motor PID
+
+  // END Stepper Motor PID
+  // **************************
+
+  // **************************
+  // For Stepper Motor Command
+  
+  StepCount = Move(dx); //one step positive
+  
+  // END Stepper Motor Command
+  // **************************
 
   delay(10);
 }
-
 
 // Bendlabs Calibration Function
 void calibrate()
@@ -90,4 +169,38 @@ void calibrate()
   myFlexSensor.calibrateX(); //Call when sensor is straight on Y axis and 90 degrees on X axis
 
   Serial.println(F("Calibration complete."));
+}
+
+//this is the function we can call to move the motor
+int Move(float dx)
+{
+  //calculate number of steps we need to move
+  int stepsaway = (dx)/DistPerStep; //calculate how many steps we need to move to get to desired location
+  int StepCount = 0;
+  StepCount += stepsaway;
+
+  if (stepsaway < 0)
+  { 
+    digitalWrite(7, HIGH); //set direction pin to high if we're going in the negative direction
+    stepsaway = stepsaway * (-1); //make this a positive value
+    delay(5); //needs a delay of 5 microseconds, this is more than that
+  }
+  //going to make an if for stepsaway being positive as well - but i can come up with a cleaner way to do this with less delay later on
+  else 
+  { 
+    digitalWrite(7, LOW); //set direction pin to high if we're going in the negative direction
+    delay(1); //needs a delay of 5 microseconds, this is more than that
+  }
+
+  //now we need to have a square wave at STEP+ pretty fast for the correct number of times for stepsaway
+  for (int i = 1; i<=stepsaway; i++)
+  {
+    digitalWrite(8, HIGH);
+    delay(5); //this could probably be less but we'll start here)
+    digitalWrite(8, LOW); //this is where it'll actually move
+    delay(5);
+    Serial.print(i);
+    Serial.println();
+  }
+  return StepCount;
 }
