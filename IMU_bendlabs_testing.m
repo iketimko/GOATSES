0cafb40e6ca04253a8569586b13c54e108826b4a
unknown <cowh9052@colorado.edugit config --global user.email cowh9052@colorado.edulscd>
2022-04-11 13:21:26 -0600

    ClosedLoop Tests

--- IMU_bendlabs_testing.m
+++ IMU_bendlabs_testing.m
@@ -11,10 +11,10 @@ close all
 %% set filenames
 
 %filename_BL = 'TestIMU';
-filename_IMU = 'LOG7.csv';
+filename_IMU = 'LOG1.csv';
 
 %% Read in bendlabs data
-%BL = readmatrix(filename_BL);
+BL = readmatrix('ClosedLoopTest2.log');
 
 %% Read in IMU data
 IMU = csvread('LOG7.csv');
@@ -25,37 +25,38 @@ IMU4 = csvread('LOG4.csv');
 IMU5 = csvread('LOG5.csv');
 IMU6 = csvread('LOG6.csv');
 
+IMU_BL = csvread('LOG14.csv');
 %Time vector for BL data
-% t_BL = 1:length(BL(:, 1));
+t_BL = 1:length(BL(:, 1));
 
 
-% %Plot BL data
-% plot(t_BL, BL(:, 1));
-% hold on
-% %plot(t_BL, BL(:, 2)); %gonna need to know how much of a second each thing is for BL
-% hold off
-% xlabel('Time [not real units currently!]');
-% ylabel('Angle [degrees]');
-% title('BendLabs data')
-% legend('Angle of ... somethin', 'Location', 'Best');
-% %k so this might not be the raw raw data i'm not sure the second data set
-% %(BL(:, 2)) can be uncommented and that's super noisy but probably also
-% %useful
-% %and we could smooth it
+%Plot BL data
+plot(t_BL, BL(:, 1));
+hold on
+%plot(t_BL, BL(:, 2)); %gonna need to know how much of a second each thing is for BL
+hold off
+xlabel('Time [not real units currently!]');
+ylabel('Angle [degrees]');
+title('BendLabs data')
+legend('Angle of ... somethin', 'Location', 'Best');
+%k so this might not be the raw raw data i'm not sure the second data set
+%(BL(:, 2)) can be uncommented and that's super noisy but probably also
+%useful
+%and we could smooth it
 
 %Plot IMU data - I don't know which axes we want so I'm gonna stick em all
 %on a plot for now
 figure
 %plot(IMU(:, 1), IMU(:, 1));
 hold on
-time = IMU(:,1)/1000;
-plot(time, IMU(:, 2));
-plot(time, IMU(:, 3));
-plot(time, IMU(:, 4));
-plot(time, IMU(:, 5));
-plot(time, IMU(:, 6));
-plot(time, IMU(:, 7));
-plot(time, IMU(:, 8));
+time = IMU_BL(:,1)/1000;
+plot(time, IMU_BL(:, 2));
+plot(time, IMU_BL(:, 3));
+plot(time, IMU_BL(:, 4));
+plot(time, IMU_BL(:, 5));
+plot(time, IMU_BL(:, 6));
+plot(time, IMU_BL(:, 7));
+plot(time, IMU_BL(:, 8));
 hold off
 xlabel('Time [ms]');
 ylabel('IMU reading [?]');
