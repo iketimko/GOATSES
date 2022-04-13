84789ada428e26c03d5a406286c5f466b91be2a6
Isaac Dunlap Timko <isti3857@colorado.edu>
2022-04-13 14:13:33 -0600

    Added Repeatability Plots

--- angle_repeatability.m
+++ angle_repeatability.m
@@ -93,8 +93,8 @@ for j = 3:6
         resultant(:,i) = R(F ,F, BL_L{j}(i,1) ,BL_R{j}(i,1));
         theta_err(i) = atand(resultant(1,i)/resultant(2,i));
     end
-
-    plot(linspace(1,length(BL_L{j})/56.1655,length(BL_L{j})), theta_err)
+    t = linspace(0,length(BL_L{j})/56.1655,length(BL_L{j}));
+    plot(t, theta_err)
 end
 
 yline(2)
@@ -103,3 +103,35 @@ yline(-2)
 title('Error in the vector angle')
 ylabel('Degrees')
 xlabel('Seconds')
+
+%% Plot the results with error
+h = figure;
+scale = 1;
+h.Position = [10 50 scale*1500 scale*700];
+hold on
+
+% define erors 
+repeatability_error = .68;
+sensor_error = .2;
+
+% Sensor error
+x_patch = [t, flip(t)];
+y_patch = [theta_err+repeatability_error+sensor_error, flip(theta_err-repeatability_error-sensor_error)];
+patch(x_patch,y_patch,'yellow','FaceAlpha',1)
+
+% Repeatability error
+x_patch = [t, flip(t)];
+y_patch = [theta_err+repeatability_error, flip(theta_err-repeatability_error)];
+patch(x_patch,y_patch,'cyan','FaceAlpha',1)
+
+% Plot the line
+plot(t,theta_err,'k','Linewidth', 2)
+
+yline(2,'r--','Linewidth', 3)
+yline(-2,'r--','Linewidth', 3)
+legend('Sensor error', 'Repeatability error', 'Vector Angle Error','Location','SE');
+ylim([-3,3])
+xlim([0,t(end)]);
+title('Error in Vector Angle','FontSize', 14);
+xlabel('Time (s)','FontSize', 12)
+ylabel('Angle Error (Deg)','FontSize', 12);
